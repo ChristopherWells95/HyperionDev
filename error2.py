0ab5928b50eb5ec432eb880ee69cbaa604e0c07c
Christopher Wells <151378901+ChristopherWells95@users.noreply.github.com>
2025-01-30 09:41:28 +0200

    Update error2.py

--- error2.py
+++ error2.py
@@ -2,10 +2,20 @@
  
 # There are some errors in this program. Run the program, look at the error messages, and find and fix the errors.
 
-animal = "Lion"
-animal_type = "cub"
+# A syntax error was found in the variable "animal", missing quotations.
+# A logical error was found in animal and animal_type and had to swop around.
+# Animal had to be declared in string form.
+
+animal = "cub"
+animal_type = "Lion"
+
+# number of teeth was set as an integer and will be left as is.
 number_of_teeth = 16
 
-full_spec = (f"This is a {animal}. It is a {number_of_teeth} and it has {animal_type} teeth")
+# full_spec variable was used to store the output of the string.
+# f-string was missing.
+# Logical error was found in the layout of the string."This is a {animal}. It is a {number_of_teeth} and it has {animal_type} teeth"
+full_spec = (f"This is a {animal}. It is a {animal_type} and it has {number_of_teeth} teeth")
 
-print (full_spec)
+# Syntax error was found in the print statement. Missing brackets.
+print(full_spec)
